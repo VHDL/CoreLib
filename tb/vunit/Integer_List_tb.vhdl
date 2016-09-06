8cf94e19d51d4f955fa87b168e859db0ed66880e
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-06 03:27:24 +0200

    Tried to add vunit.

--- tb/vunit/Integer_List_tb.vhdl
+++ tb/vunit/Integer_List_tb.vhdl
@@ -30,39 +30,70 @@
 library CoreLib;
 use			work.DataStructures.all;
 
+library vunit_lib;
+context vunit_lib.vunit_context;
+
 entity DataStructures_Integer_List_tb is
+	generic (runner_cfg : string);
 end entity;
 
 architecture test of DataStructures_Integer_List_tb is
-	-- shared variable	GlobalStdOut	: T_STDOUT;
-	shared variable List1					: Integer_List;
+
 begin
-	process
-		variable index		: INTEGER;
-		variable element	: INTEGER;
+	TestRunner : process
+		variable start     : time;
+		variable timed_out : boolean;
+		
+		variable index     : INTEGER;
+		variable element   : INTEGER;
+		
+		variable List1     : Integer_List;
 	begin
+		checker_init(display_format => verbose);
+		test_runner_setup(runner, runner_cfg);
+
 		List1.Init;
 		
-		for i in 0 to 67 loop
-			element := i + 1;
-			GlobalStdOut.PrintLine("Append " & INTEGER'image(element));
-			index		:= List1.Append(element);
-			GlobalStdOut.PrintLine("  index= " & INTEGER'image(index));
-			
-			index		:= List1.IndexOf(element);
-			List1.Set(index, element + 100);
-			element	:= List1.Get(i);
-			GlobalStdOut.PrintLine("  IndexOf -> " & INTEGER'image(index));
-			GlobalStdOut.PrintLine("  Get -> " & INTEGER'image(element));
+		while test_suite loop
+			if run("testcase 1") then
+				null;
+			elsif run("testcase 2") then
+				null;
+			end if;
 		end loop;
-		for i in 54 downto 27 loop
-			GlobalStdOut.PrintLine("RemoveAt " & INTEGER'image(i));
-			List1.RemoveAt(i);
-		end loop;
-		for i in 20 downto 7 loop
-			GlobalStdOut.PrintLine("Remove " & INTEGER'image(i + 101));
-			List1.Remove(i + 101);
-		end loop;
+
+		test_runner_cleanup(runner);
 		wait;
 	end process;
+
+	test_runner_watchdog(runner, 100 ns);
+			
+	-- process
+		-- variable index		: INTEGER;
+		-- variable element	: INTEGER;
+	-- begin
+		-- List1.Init;
+		
+		-- for i in 0 to 67 loop
+			-- element := i + 1;
+			-- GlobalStdOut.PrintLine("Append " & INTEGER'image(element));
+			-- index		:= List1.Append(element);
+			-- GlobalStdOut.PrintLine("  index= " & INTEGER'image(index));
+			
+			-- index		:= List1.IndexOf(element);
+			-- List1.Set(index, element + 100);
+			-- element	:= List1.Get(i);
+			-- GlobalStdOut.PrintLine("  IndexOf -> " & INTEGER'image(index));
+			-- GlobalStdOut.PrintLine("  Get -> " & INTEGER'image(element));
+		-- end loop;
+		-- for i in 54 downto 27 loop
+			-- GlobalStdOut.PrintLine("RemoveAt " & INTEGER'image(i));
+			-- List1.RemoveAt(i);
+		-- end loop;
+		-- for i in 20 downto 7 loop
+			-- GlobalStdOut.PrintLine("Remove " & INTEGER'image(i + 101));
+			-- List1.Remove(i + 101);
+		-- end loop;
+		-- wait;
+	-- end process;
 end architecture;
