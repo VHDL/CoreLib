95efef934e4d5e807185c6b96bb8b0a070107d93
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-14 03:43:58 +0200

    Added first implementations of synthesizable functions.

---
+++ run.py
@@ -0,0 +1,19 @@
+from vunit import VUnit
+from pathlib import Path
+
+rootDirectory = Path.cwd()
+
+# Create and configure a VUnit instance and handover the command line arguments
+vu = VUnit.from_argv()
+vu.enable_location_preprocessing()
+
+# Create the VHDL library 'CoreLib' and add required files
+CoreLib = vu.add_library("CoreLib")
+CoreLib.add_source_files(str(rootDirectory / "src/components.vhdl"))
+
+# Create the VHDL library 'Testbench' and add required files
+# Testbench = vu.add_library("Testbench")
+CoreLib.add_source_files(str(rootDirectory / "tb/vunit/components_tb.vhdl"))
+
+# launch VUnit
+vu.main()
