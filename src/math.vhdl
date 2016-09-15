be1974e1882f848b8a5f0ceaa2748039813ba438
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-09-16 01:34:29 +0200

    Math: Removed tabs. Replaced `check` with `check_equal`.

--- src/math.vhdl
+++ src/math.vhdl
@@ -1,14 +1,11 @@
--- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
--- vim: tabstop=2:shiftwidth=2:noexpandtab
--- kate: tab-width 2; replace-tabs off; indent-width 2;
 -- =============================================================================
--- Authors:					Patrick Lehmann
+-- Authors:          Patrick Lehmann
 --
--- Package:					Math extension package.
+-- Package:          Math extension package.
 --
 -- Description:
 -- -------------------------------------
---		This package provides additional math functions.
+--    This package provides additional math functions.
 --
 -- License:
 -- =============================================================================
@@ -18,7 +15,7 @@
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
 --
---		http://www.apache.org/licenses/LICENSE-2.0
+--    http://www.apache.org/licenses/LICENSE-2.0
 --
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,75 +26,75 @@
 
 
 package math is
-	-- figurate numbers
-	function squareNumber(N : natural) return natural;
-	function cubicNumber(N : natural) return natural;
-	function triangularNumber(N : natural) return natural;
-	
-	-- coefficients
-	-- binomial coefficient (N choose K)
-	function binomialCoefficient(N : positive; K : positive) return positive;
-	
-	-- greatest common divisor (gcd)
-	function greatestCommonDivisor(N1 : positive; N2 : positive) return positive;
-	-- least common multiple (lcm)
-	function leastCommonMultiple(N1 : positive; N2 : positive) return positive;
+  -- figurate numbers
+  function squareNumber(N : natural) return natural;
+  function cubicNumber(N : natural) return natural;
+  function triangularNumber(N : natural) return natural;
+  
+  -- coefficients
+  -- binomial coefficient (N choose K)
+  function binomialCoefficient(N : positive; K : positive) return positive;
+  
+  -- greatest common divisor (gcd)
+  function greatestCommonDivisor(N1 : positive; N2 : positive) return positive;
+  -- least common multiple (lcm)
+  function leastCommonMultiple(N1 : positive; N2 : positive) return positive;
 end package;
 
 
 package body math is
-	-- figurate numbers
-	function squareNumber(N : natural) return natural is
-	begin
-		return N ** 2;
-	end function;
-	
-	function cubicNumber(N : natural) return natural is
-	begin
-		return N ** 3;
-	end function;
-	
-	function triangularNumber(N : natural) return natural is
-		variable T	: natural;
-	begin
-		return (N * (N + 1) / 2);
-	end function;
-	
-	-- coefficients
-	function binomialCoefficient(N : positive; K : positive) return positive is
-		variable Result		: positive;
-	begin
-		Result		:= 1;
-		for i in 1 to K loop
-			Result := Result * (((N + 1) - i) / i);
-		end loop;
-		return Result;
-	end function;
-	
-	-- greatest common divisor (gcd)
-	function greatestCommonDivisor(N1 : positive; N2 : positive) return positive is
-		variable M1				: positive;
-		variable M2				: natural;
-		variable Remainer	: natural;
-	begin
-		if (N1 >= N2) then
-			M1 := N1;
-			M2 := N2;
-		else
-			M1 := N2;
-			M2 := N1;
-		end if;
-		while M2 /= 0 loop
-			Remainer	:= M1 mod M2;
-			M1				:= M2;
-			M2				:= Remainer;
-		end loop;
-		return M1;
-	end function;
-	
-	-- least common multiple (lcm)
-	function leastCommonMultiple(N1 : positive; N2 : positive) return positive is
-	begin
-		return ((N1 * N2) / greatestCommonDivisor(N1, N2));
-	end function;
+  -- figurate numbers
+  function squareNumber(N : natural) return natural is
+  begin
+    return N ** 2;
+  end function;
+  
+  function cubicNumber(N : natural) return natural is
+  begin
+    return N ** 3;
+  end function;
+  
+  function triangularNumber(N : natural) return natural is
+    variable T  : natural;
+  begin
+    return (N * (N + 1) / 2);
+  end function;
+  
+  -- coefficients
+  function binomialCoefficient(N : positive; K : positive) return positive is
+    variable Result    : positive;
+  begin
+    Result    := 1;
+    for i in 1 to K loop
+      Result := Result * (((N + 1) - i) / i);
+    end loop;
+    return Result;
+  end function;
+  
+  -- greatest common divisor (gcd)
+  function greatestCommonDivisor(N1 : positive; N2 : positive) return positive is
+    variable M1        : positive;
+    variable M2        : natural;
+    variable Remainer  : natural;
+  begin
+    if (N1 >= N2) then
+      M1 := N1;
+      M2 := N2;
+    else
+      M1 := N2;
+      M2 := N1;
+    end if;
+    while (M2 /= 0) loop
+      Remainer  := M1 mod M2;
+      M1        := M2;
+      M2        := Remainer;
+    end loop;
+    return M1;
+  end function;
+  
+  -- least common multiple (lcm)
+  function leastCommonMultiple(N1 : positive; N2 : positive) return positive is
+  begin
+    return ((N1 * N2) / greatestCommonDivisor(N1, N2));
+  end function;
 end package body;
--- tb/vunit/math_tb.vhdl
+++ tb/vunit/math_tb.vhdl
@@ -1,14 +1,11 @@
--- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
--- vim: tabstop=2:shiftwidth=2:noexpandtab
--- kate: tab-width 2; replace-tabs off; indent-width 2;
 -- =============================================================================
--- Authors:					Patrick Lehmann
+-- Authors:          Patrick Lehmann
 --
--- Package:					Math extension package.
+-- Package:          Math extension package.
 --
 -- Description:
 -- -------------------------------------
---		This package provides additional math functions.
+--    This package provides additional math functions.
 --
 -- License:
 -- =============================================================================
@@ -18,7 +15,7 @@
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
 --
---		http://www.apache.org/licenses/LICENSE-2.0
+--    http://www.apache.org/licenses/LICENSE-2.0
 --
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
@@ -28,38 +25,38 @@
 -- =============================================================================
 
 library CoreLib;
-use			work.math.all;
+use     work.math.all;
 
 library vunit_lib;
 context vunit_lib.vunit_context;
 
 
 entity math_tb is
-	generic (runner_cfg : string);
+  generic (runner_cfg : string);
 end entity;
 
 
 architecture test of math_tb is
 begin
-	TestRunner : process
-	begin
-		test_runner_setup(runner, runner_cfg);
+  TestRunner : process
+  begin
+    test_runner_setup(runner, runner_cfg);
 
-		while test_suite loop
-			if run("Check square numbers from 0 to 20") then
-				for i in 0 to 20 loop
-					check(squareNumber(i) = (i*i));
-				end loop;
-			elsif run("Check cubic numbers from 0 to 20") then
-				for i in 0 to 20 loop
-					check(cubicNumber(i) = (i*i*i));
-				end loop;
-			end if;
-		end loop;
+    while test_suite loop
+      if run("Check square numbers from 0 to 20") then
+        for i in 0 to 20 loop
+          check_equal(squareNumber(i), (i*i));
+        end loop;
+      elsif run("Check cubic numbers from 0 to 20") then
+        for i in 0 to 20 loop
+          check_equal(cubicNumber(i), (i*i*i));
+        end loop;
+      end if;
+    end loop;
 
-		test_runner_cleanup(runner);
-		wait;
-	end process;
+    test_runner_cleanup(runner);
+    wait;
+  end process;
 
-	test_runner_watchdog(runner, 100 ns);
+  test_runner_watchdog(runner, 100 ns);
 end architecture;
